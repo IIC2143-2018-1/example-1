ea24b44cfcae8135aab9e26b873fe03c33d6426e
Andrés Fernández <alfernandez@uc.cl>
2018-04-25 21:21:17 -0300

    Added devise model User

--- Gemfile
+++ Gemfile
@@ -6,6 +6,8 @@ git_source(:github) do |repo_name|
 end
 
 
+gem 'devise'
+
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.1.5'
 # Use postgresql as the database for Active Record
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
+  get 'user/info', to: 'users#info'
+
+  devise_for :users
   root 'welcome#index'
+
+  # get 'artists/:id', to: 'artists#show', as: 'cualquiercosa'
+
+  resources :artists, :albums, :songs
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
